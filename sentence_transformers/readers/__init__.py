829c7170e112ed0d4e0ff3e5574555ca2b38a5d3
utkuumur <uuacikalin@gmail.com>
2020-01-07 02:28:59 +0300

    update

--- sentence_transformers/readers/__init__.py
+++ sentence_transformers/readers/__init__.py
@@ -2,4 +2,5 @@ from .InputExample import InputExample
 from .LabelSentenceReader import LabelSentenceReader
 from .NLIDataReader import NLIDataReader
 from .STSDataReader import STSDataReader
+from .PatentDataReader import PatentDataReader
 from .TripletReader import TripletReader
