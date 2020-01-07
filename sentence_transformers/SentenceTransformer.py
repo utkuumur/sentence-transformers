743fa9ebee4f3fbb2cf9e576f87df6cef33a9881
utkuumur <uuacikalin@gmail.com>
2020-01-07 03:29:43 +0300

    update

--- sentence_transformers/SentenceTransformer.py
+++ sentence_transformers/SentenceTransformer.py
@@ -380,7 +380,7 @@ class SentenceTransformer(nn.Sequential):
                         loss_model.train()
 
             if save_epoch:
-                self.save(output_path)
+                self.save(output_path+ "_"+epoch)
 
             self._eval_during_training(evaluator, output_path, save_best_model, epoch, -1)
 
