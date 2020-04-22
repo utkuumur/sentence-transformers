ae753a466f19ee8f1974d95b6ce41a4724da617c
Utku <uuacikalin@gmail.com>
2020-04-22 05:33:38 +0300

    faster tokenizing for roberta

--- sentence_transformers/models/RoBERTa.py
+++ sentence_transformers/models/RoBERTa.py
@@ -1,6 +1,6 @@
 from torch import Tensor
 from torch import nn
-from transformers import RobertaModel, RobertaTokenizer
+from transformers import RobertaModel, RobertaTokenizer,RobertaTokenizerFast
 import json
 from typing import Union, Tuple, List, Dict
 import os
@@ -17,25 +17,30 @@ class RoBERTa(nn.Module):
         self.config_keys = ['max_seq_length', 'do_lower_case']
         self.do_lower_case = do_lower_case
 
-        if max_seq_length > 511:
+        if max_seq_length > 512:
             logging.warning("RoBERTa only allows a max_seq_length of 511 (514 with special tokens). Value will be set to 511")
-            max_seq_length = 511
+            max_seq_length = 512
         self.max_seq_length = max_seq_length
 
 
         self.roberta = RobertaModel.from_pretrained(model_name_or_path)
         self.tokenizer = RobertaTokenizer.from_pretrained(model_name_or_path, do_lower_case=do_lower_case)
+        self.fast_tokenizer = RobertaTokenizerFast.from_pretrained(model_name_or_path, do_lower_case=do_lower_case, add_special_tokens=False)
         self.cls_token_id = self.tokenizer.convert_tokens_to_ids([self.tokenizer.cls_token])[0]
         self.sep_token_id = self.tokenizer.convert_tokens_to_ids([self.tokenizer.sep_token])[0]
 
     def forward(self, features):
         """Returns token_embeddings, cls_token"""
-        #RoBERTa does not use token_type_ids
-        output_tokens = self.roberta(input_ids=features['input_ids'], token_type_ids=None, attention_mask=features['input_mask'])[0]
+        output_states = self.roberta(**features)
+        output_tokens = output_states[0]
         cls_tokens = output_tokens[:, 0, :]  # CLS token is first token
-        features.update({'token_embeddings': output_tokens, 'cls_token_embeddings': cls_tokens, 'input_mask': features['input_mask']})
-        return features
+        features.update({'token_embeddings': output_tokens, 'cls_token_embeddings': cls_tokens, 'attention_mask': features['attention_mask']})
 
+        if len(output_states) > 2:
+            features.update({'all_layer_embeddings': output_states[2]})
+
+        return features
+        
     def get_word_embedding_dimension(self) -> int:
         return self.roberta.config.hidden_size
 
@@ -43,39 +48,22 @@ class RoBERTa(nn.Module):
         """
         Tokenizes a text and maps tokens to token-ids
         """
-        return self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(text))
+        #return self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(text))
+        #fast_tokenizer
+        return self.fast_tokenizer.convert_tokens_to_ids(self.fast_tokenizer.tokenize(text))[1:-1]
+
 
     def get_sentence_features(self, tokens: List[int], pad_seq_length: int):
         """
         Convert tokenized sentence in its embedding ids, segment ids and mask
-
         :param tokens:
             a tokenized sentence
         :param pad_seq_length:
             the maximal length of the sequence. Cannot be greater than self.sentence_transformer_config.max_seq_length
         :return: embedding ids, segment ids and mask for the sentence
         """
-        pad_seq_length = min(pad_seq_length, self.max_seq_length)
-
-        tokens = tokens[:pad_seq_length]
-        input_ids = [self.cls_token_id] + tokens + [self.sep_token_id] + [self.sep_token_id]
-        sentence_length = len(input_ids)
-
-        pad_seq_length += 3  ##Add Space for CLS + SEP + SEP token
-
-        input_mask = [1] * len(input_ids)
-
-        # Zero-pad up to the sequence length. BERT: Pad to the right
-        padding = [0] * (pad_seq_length - len(input_ids))
-        input_ids += padding
-
-        input_mask += padding
-
-        assert len(input_ids) == pad_seq_length
-        assert len(input_mask) == pad_seq_length
-
-
-        return {'input_ids': np.asarray(input_ids, dtype=np.int64), 'input_mask': np.asarray(input_mask, dtype=np.int64), 'sentence_lengths': np.asarray(sentence_length, dtype=np.int64)}
+        pad_seq_length = min(pad_seq_length, self.max_seq_length) + 2 ##Add Space for CLS + SEP token
+        return self.tokenizer.prepare_for_model(tokens, max_length=pad_seq_length, pad_to_max_length=True, return_tensors='pt')
 
     def get_config_dict(self):
         return {key: self.__dict__[key] for key in self.config_keys}
